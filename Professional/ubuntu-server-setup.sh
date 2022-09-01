14b25e2f782c45842f0fff495e6c53fd849c01fb
sam <samuel.troyer.j@gmail.com>
2022-08-31 18:48:37 -0600

    Updates to ubuntu-server-setup.sh.

--- Professional/ubuntu-server-setup.sh
+++ Professional/ubuntu-server-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 read -p "Username:  " user
-adduser $user && usermod -aG sudo $user
-ufw allow OpenSSH && ufw enable && ufw status
-echo 'Reboot into $user'
+if [ ! user = "no" ]; then
+        adduser $user && usermod -aG sudo $user
+fi
+sudo bash -c "ufw allow OpenSSH && ufw enable"
+ufw status
+sudo bash -c "apt-get update && apt-get upgrade && apt-get install vim sudo curl git golang-go build-essentials"
+sudo reboot
